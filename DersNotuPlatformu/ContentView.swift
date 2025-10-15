f42c8f0e8dbebe801e1cefd7674a1225e83b0064
wFrkn <frknylmzw00@gmail.com>
2025-10-15 16:50:57 +0300

    Initial Commit

---
+++ DersNotuPlatformu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DersNotuPlatformu
+//
+//  Created by Furkan Yılmaz on 15.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DersNotuPlatformu/DersNotuPlatformuApp.swift
@@ -0,0 +1,17 @@
+//
+//  DersNotuPlatformuApp.swift
+//  DersNotuPlatformu
+//
+//  Created by Furkan Yılmaz on 15.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct DersNotuPlatformuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
